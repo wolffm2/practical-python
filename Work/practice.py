d5d413b4ab5cdefcb6ec69255e73ab4d8c1b3b5f
wolffm2 <62778749+wolffm2@users.noreply.github.com>
2020-08-02 17:57:04 -0700

    upload work on Practical Python course

---
+++ Work/practice.py
@@ -0,0 +1,7 @@
+x = 100
+print(x)
+
+name = 'Jake'
+print('Hello ', end='') # end'' suppresses a new line
+print('My name is', name) # print the text 'My name is Jake'
+print()#puts a newline at the end
